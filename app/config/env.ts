319fa507d2403c371ab4bbbd39ff4e13973c9583
Rahul K <rahulk@iprogrammer.com>
2019-05-14 15:34:55 +0530

    Changes

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,14 +1,14 @@
 const env = {
-    database: 'leadx_poc',
-    username: 'root',
-    password: 'root',
-    host: 'localhost',
-    dialect: 'mysql',
+    database: "leadx_poc",
+    username: "root",
+    password: "root",
+    host: "localhost",
+    dialect: "mysql",
     pool: {
-      max: 5,
+      max : 5,
       min: 0,
       acquire: 30000,
-      idle: 10000
-    }
+      idle: 10000,
+    },
   };
   
