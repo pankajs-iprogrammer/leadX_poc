6358f89eaf4ed4c1ee68073eea9e910cc98eaf3c
Rahul K <rahulk@iprogrammer.com>
2019-05-23 16:40:46 +0530

    Changes

---
+++ app/components/master/activityType.model.ts
@@ -0,0 +1,47 @@
+import * as Sequelize from "sequelize";
+import { CONSTANTS } from "../../config/constants";
+import db from "../../config/db.config";
+
+const sObj = db.sObj;
+
+const ActivityType = sObj.define(
+    "activity_type",
+    {
+        name: {
+            type: Sequelize.STRING(CONSTANTS.HUNDRED),
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [CONSTANTS.ZERO, CONSTANTS.HUNDRED]
+            }
+        }
+    },
+    {
+        underscored: true
+    }
+);
+/*
+LeadStatus.sync()
+    .then(function() {
+        // Table created
+        LeadStatus.create({
+            name: "Lead"
+        });
+        LeadStatus.create({
+            name: "Opportunity"
+        });
+        LeadStatus.create({
+            name: "Negotiation"
+        });
+        LeadStatus.create({
+            name: "Proposal"
+        });
+        LeadStatus.create({
+            name: "Closed"
+        });
+    })
+    .then(c => {
+        console.log("Created user Stat", c.toJSON());
+    })
+    .catch(e => console.error(e));*/
+export default ActivityType;
